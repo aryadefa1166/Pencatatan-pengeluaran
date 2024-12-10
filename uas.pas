7ce7694dca22036770b45f9fdb351e4c10ec418c
rio-johanes <riojohanes08@gmail.com>
2024-12-10 08:52:59 +0700

    Update uas.pas
    procedure tambah

--- uas.pas
+++ uas.pas
@@ -12,3 +12,18 @@ var
     data: array[1..100] of pengeluaran;
     anggaran, total, n: longint;
     menu: integer;
+
+procedure tambah; //menambah data pengeluaran
+var
+    p: pengeluaran;
+begin
+    clrscr;
+    with p do
+    begin
+        write('Masukkan jenis pengeluaran: ');
+        readln(jenis);
+        write('Masukkan jumlah pengeluaran (dalam angka): ');
+        readln(jumlah);
+        write('Masukkan tanggal pengeluaran (dd-mm-yyyy): ');
+        readln(tanggal);
+    end;
